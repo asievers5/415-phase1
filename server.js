12e7f49c4f5c851d11cc79c78406b81c08b36ea2
Andrew Sievers <andrew.sievers@selu.edu>
2019-03-26 18:32:24 -0500

    Added an example ticket

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var chalk = require('chalk');
 var app = express();
 var router = express.Router();
 var port = process.env.PORT || 80;
+var tickets = {
+    "id": 35436,
+    "created_at": "2015-07-20T22:55:29Z",
+    "updated_at": "2016-05-05T10:38:52Z",
+    "type": "incident",
+    "subject": "MFP not working right",
+    "description": "PC Load Letter? What does that even mean???",
+    "priority": "med",
+    "status": "open",
+    "recipient": "support_example@selu.edu",
+    "submitter": "Michael_bolton@selu.edu",
+    "assignee_id": 235323,
+    "follower_ids": [235323, 234],
+    "tags": ["enterprise", "printers"],
+   }
 
 //This is the section where it will access an endpoint such as:
 //https://desolate-atoll-59781.herokuapp.com/api/test (note /api/test/)
@@ -11,7 +26,11 @@ router.get('/test', function(req, res) {
 });
 
 router.get('/list', function(req, res) {
-    res.status(200).send("SUCCESS");
+    res.status(200).send(tickets);
+
+});
+
+router.post('/ticket', function(req, res){
 
 });
 
