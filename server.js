1bf531a946ab2e22d4341f5bcf5e4245d2c1131a
Andrew Sievers <andrew.sievers@selu.edu>
2019-04-08 19:06:38 -0500

    apparently we needed a whole bunch of fields so i added a whole bunch of fields

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ var port = process.env.PORT || 80;
 var bodyParser = require('body-parser');
 var tickets = [{
         "id": 1,
-        "owner" : "KC Sievers",
-        "subject" : "Math homework due"},
-    {
-        "id": 2,
-        "owner" : "Andrew Sievers",
-        "subject" : "415 homework"
+        "created_at" : "KC Sievers",
+        "updated_at" : "Math homework due",
+        "type": "homework",
+        "subject": "Math",
+        "description": "Really hard stuff from calculus",
+        "priority": "urgent",
+        "status": "incomplete",
+        "recipient": "Math teacher",
+        "submitter": "kc",
+        "assignee_id": 123,
+        "follower_ids": [123, 1],
+        "tags": ["school", "homework"]
     }];
 
 // GET https://polar-castle-32257.herokuapp.com/rest/list
@@ -29,29 +35,49 @@ router.get('/', function(req, res) {
 // parameters sent with 
 router.post('/ticket', function(req, res) {
     var id = req.body.id;
-    var owner = req.body.owner;
+    var created_at = req.body.created_at;
+    var updated_at = req.body.updated_at;
+    var type = req.body.type;
     var subject = req.body.subject;
+    var description = req.body.description;
+    var priority = req.body.priority;
+    var status = req.body.status;
+    var recipient = req.body.recipient;
+    var submitter = req.body.submitter;
+    var assignee_id = req.body.assignee_id;
+    var follower_ids = req.body.follower_ids;
+    var tags = req.body.tags;
     var ticket = {
         id,
-        owner,
-        subject
-    }
+        created_at,
+        updated_at,
+        type,
+        subject,
+        description,
+        priority,
+        status,
+        recipient,
+        submitter,
+        assignee_id,
+        follower_ids,
+        tags
+        }
     tickets.push(ticket);
 
-    res.send(id + ' ' + owner + ' ' + subject);
+    res.send("POST Successfull");
 });
 
 router.get('/ticket/:id', function(req, res) {
     for(var i = 0; i < tickets.length; i++){
         if(tickets[i].id == req.params.id) {
-            status = tickets[i];
+            sendjson = tickets[i];
             break;
         }
         else {
-            status = "Ticket not found";
+            sendjson = "Ticket not found";
         }
     }
-    res.status(200).send(status);
+    res.status(200).send(sendjson);
 });
 
 //express.use() 
